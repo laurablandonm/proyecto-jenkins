2ab08b4e8ad43d72e09e8af7a9fd292be43a3df1
Laura Blandon <laurablandonm@gmail.com>
2024-06-09 22:24:06 -0500

    correcion MONGO_URI

--- api-escalamiento-proyecto-servicio/config.js
+++ api-escalamiento-proyecto-servicio/config.js
@@ -7,5 +7,6 @@ export const ATLAS_PASSWORD = process.env.ATLAS_PASSWORD;
 export const ATLAS_USER = process.env.ATLAS_USER;
 export const ATLAS_APP_NAME  = process.env.ATLAS_APP_NAME ;
 export const DB_NAME = process.env.DB_NAME;
+export const MONGO_URI = process.env.MONGO_URI;
 
 
--- api-escalamiento-proyecto-servicio/db/mongo_connection.js
+++ api-escalamiento-proyecto-servicio/db/mongo_connection.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import {ATLASURL} from "../utils/utils.js"
+import {MONGO_URI} from "../config.js"
 
 export const getConexion = async () => {
     try {
-        await mongoose.connect(ATLASURL)
+        await mongoose.connect(MONGO_URI)
         console.log("conectado a base de datos")    
     } catch (error) {
         console.log(error)   
